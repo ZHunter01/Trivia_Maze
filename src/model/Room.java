069d54f020966d994f2bc395ee8378c87b9c8edf
Zach Hunter <zachhunter01@gmail.com>
2021-05-13 17:48:13 -0700

    Bug fixes in room

--- src/model/Room.java
+++ src/model/Room.java
@@ -1,14 +1,11 @@
 package model;
 
-import javax.swing.ImageIcon;
-
 public class Room {
-    private Room myRoom;
+    private final static Room MY_ROOM = new Room();
     private Door myDoorUp;
     private Door myDoorLeft;
     private Door myDoorRight;
     private Door myDoorDown;
-    //Int Values 0 -- 3 determine which way user is trying to go
     /**Int value to indicate up door is selected */
     public final static int UP = 0;
     /**Int value to indicate left door is selected */
@@ -17,46 +14,23 @@ public class Room {
     public final static int DOWN = 2;
     /**Int value to indicate right door is selected */
     public final static int RIGHT = 3;
-    /**ImageIcon to display room object */
-    private ImageIcon myIcon;
     
-    /** Creates default Room object with four Doors
-     * 
-     */
     public Room() {
         myDoorUp = new Door();
         myDoorLeft = new Door();
         myDoorRight = new Door();
         myDoorDown = new Door();
-        myRoom = new Room();
-        myIcon = new ImageIcon();
+        //myRoom = new Room();
     }
     
-    /** Return current state of Room object
-     * 
-     * @return myRoom
-     */
     public Room getRoom() {
-        return myRoom;
+        return MY_ROOM;
     }
     
-    /** Get current Room ImageIcon
-     * 
-     * @return myIcon
-     */
-    public ImageIcon getRoomIcon() {
-        return myIcon;
-    }
     
-    /** Get Door from which direction user is trying to go
-     * 
-     * @param theDir
-     * @return userDoor
-     */
     public Door getUserDoor(final int theDir) {
         Door userDoor = new Door();
         
-        //Get appropriate door
         if (theDir == UP) {
             userDoor = myDoorUp; 
         } else if (theDir == LEFT) {
@@ -65,18 +39,13 @@ public class Room {
             userDoor = myDoorDown; 
         } else if (theDir == RIGHT) {
             userDoor = myDoorRight; 
-        } else { //If value not within 0 to 3 is passed, throw exception
+        } else {
             throw new IllegalArgumentException("Error: Parameter must be an int value from 0 to 3");
         }
         
         return userDoor;
     }
     
-    /** Get lock state of door user is trying to go through
-     * 
-     * @param theDir
-     * @return userLock
-     */
     public boolean getDoorLock(final int theDir) {
         boolean userLock = false;
         
@@ -95,11 +64,6 @@ public class Room {
         return userLock;
     }
     
-    /** Return if specified door is perma locked
-     * 
-     * @param theDir
-     * @return userLock
-     */
     public boolean getDoorPermaLock(final int theDir) {
         boolean userLock = false;
         
@@ -116,14 +80,6 @@ public class Room {
         }
         
         return userLock;
-    }
-    
-    /** Sets room ImageIcon to input ImageIcon
-     * 
-     * @param theIcon
-     */
-    public void setRoomIcon(final ImageIcon theIcon) {
-        myIcon = theIcon;
     }
     
 }
