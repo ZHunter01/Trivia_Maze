81d6a8d36e0b6886223c1eafb82087d8b38750fc
msanyok <msanyoks@yandex.ua>
2021-05-17 00:57:18 -0700

    Was added needed methods to SqliteDB class.

--- src/db/DataBase.java
+++ src/db/DataBase.java
@@ -1,5 +1,15 @@
+/**
+ * Trivia Maze TCSS 360 Spring 2021
+ */
+
 package db;
 
+/**
+ * This class is only for demonstrate all functions of SqliteDB.java class
+ * 
+ * @author Oleksandr Maistruk 
+ *
+ */
 public class Database {
     
     
@@ -7,6 +17,12 @@ public class Database {
         SqliteDB db = new SqliteDB();
         db.listQuestions();
 //        db.InsertToDB("Sky is", "blue");
+        System.out.println(db.getQuestion(1));
+        System.out.println(db.getAnswer(1));
+        db.setIsUsedToDefault();
+        System.out.println("Is Used: " + db.getIsUsed(2));
+        System.out.println("Last ID: " + db.getLastId());
+//        db.updateIsUsed(1);
         db.CloseDB();
      }
 }
--- src/db/SqliteDB.java
+++ src/db/SqliteDB.java
@@ -1,3 +1,7 @@
+/**
+ * Trivia Maze TCSS 360 Spring 2021
+ */
+
 package db;
 
 import java.sql.Connection;
@@ -5,6 +9,12 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+
+/**
+ * 
+ * @author Oleksandr Maistruk 
+ *
+ */
 public class SqliteDB {
     Connection c = null;
     Statement stmt = null;
@@ -28,9 +38,9 @@ public class SqliteDB {
             ResultSet rs = stmt.executeQuery("SELECT * FROM QuestionTable");
             
             while(rs.next()) {
-                int id = rs.getInt("ID");
-                String question = rs.getString("Question");
-                String answer = rs.getString("Answer");
+                final int id = rs.getInt("ID");
+                final String question = rs.getString("Question");
+                final String answer = rs.getString("Answer");
                 System.out.println("ID: " + id + "Question: " + question + "Answer: " + answer);
                 
             }
@@ -39,20 +49,88 @@ public class SqliteDB {
          }
     }
     
-    public String getQuestion(int id) {
+    public String getQuestion(final int theId) {
         String question;
         try {
             this.stmt = c.createStatement();
             
-            ResultSet rs = stmt.executeQuery("select Question from QuestionTable where id = " + id);  
+            final ResultSet rs = stmt.executeQuery("select Question from QuestionTable where id = " + theId);  
             question = rs.getString("Question");
             return question; 
             
         } catch ( Exception e ) {
             System.out.println("Error: " + e.getMessage() );
-         }
+        }
         
        return "There is no question";
+    }
+    
+    public String getAnswer(int id) {
+        String question;
+        try {
+            this.stmt = c.createStatement();
+            
+            ResultSet rs = stmt.executeQuery("select Answer from QuestionTable where id = " + id);  
+            question = rs.getString("Answer");
+            return question; 
+            
+        } catch ( Exception e ) {
+            System.out.println("Error: " + e.getMessage() );
+        }
+        
+       return "There is no answer";
+    }
+    
+    public void updateIsUsed(int id) {
+        try {
+            this.stmt = c.createStatement();
+            String query = "UPDATE QuestionTable SET IsUsed = 1 WHERE id=" + id;
+            stmt.executeUpdate(query);
+//            System.out.println("Updated.");
+            
+        } catch ( Exception e ) {
+            System.out.println("Error: " + e.getMessage() );
+        }
+    }
+    
+    /**
+     * The method to check if a question used or no.
+     * 
+     * @param id is the question id.
+     * @return true if question already used
+     * @return false if question doesn't use 
+     */
+    public boolean getIsUsed(int id) {
+        String isUsed;
+        try {
+            this.stmt = c.createStatement();
+            
+            ResultSet rs = stmt.executeQuery("select IsUsed from QuestionTable where id = " + id);  
+            isUsed = rs.getString("IsUsed");
+//            System.out.println("Isused: " + isUsed);
+            if (isUsed.equals("1")) {
+                return true;
+            }
+        } catch ( Exception e ) {
+            System.out.println("Error: " + e.getMessage() );
+        }
+        return false;
+    }
+    
+    public void setIsUsedToDefault() {
+        try {
+            this.stmt = c.createStatement();
+            
+            ResultSet rs = stmt.executeQuery("select * from QuestionTable");
+            String query = "UPDATE QuestionTable SET IsUsed = 0";
+            while(rs.next()) {
+                stmt.executeUpdate(query);
+//                System.out.println("Changed.");
+            } 
+            
+        } catch ( Exception e ) {
+            System.out.println("Error: " + e.getMessage() );
+        }
     }
     
     public void InsertToDB(String Q, String A) {
@@ -63,7 +141,23 @@ public class SqliteDB {
             stmt.executeUpdate(sql);        
         } catch ( Exception e ) {
             System.out.println("Error: " + e.getMessage() );
-         }
+        }
+    }
+    
+    public int getLastId() {
+        int id = 0;
+        String lastRow = "SELECT * FROM QuestionTable WHERE ID = (SELECT MAX(ID) FROM QuestionTable)";
+        
+        try {
+            this.stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery(lastRow);  
+            id = rs.getInt("ID");
+            
+//            System.out.println("get last id: " +id);        
+        } catch ( Exception e ) {
+            System.out.println("Error: " + e.getMessage() );
+        }
+        return id;
     }
     
     public void CloseDB() {
