4469a4167e53ab3883cac1c9dd332795ff4a5eda
Zach Hunter <zachhunter01@gmail.com>
2021-05-18 12:47:50 -0700

    Added database code

--- src/db/SqliteDB.java
+++ src/db/SqliteDB.java
@@ -1,3 +1,8 @@
+
+/**
+ * Trivia Maze TCSS 360 Spring 2021
+ */
+
 package db;
 
 import java.sql.Connection;
@@ -5,20 +10,26 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+
+/**
+ * 
+ * @author Oleksandr Maistruk 
+ *
+ */
 public class SqliteDB {
-    Connection c = null;
-    Statement stmt = null;
+    private Connection c = null;
+    private Statement stmt = null;
     
     public SqliteDB() {
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:QuestionsDB.db");
+    try {
+       Class.forName("org.sqlite.JDBC");
+       c = DriverManager.getConnection("jdbc:sqlite:QuestionsDB.db");
        
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
-        }
-        System.out.println("Opened database successfully");
+    } catch ( Exception e ) {
+       System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+       System.exit(0);
+    }
+//    System.out.println("Opened database successfully");
     
     }
     
@@ -28,38 +39,136 @@ public class SqliteDB {
             ResultSet rs = stmt.executeQuery("SELECT * FROM QuestionTable");
             
             while(rs.next()) {
-                int id = rs.getInt("ID");
-                String question = rs.getString("Question");
-                String answer = rs.getString("Answer");
+                final int id = rs.getInt("ID");
+                final String question = rs.getString("Question");
+                final String answer = rs.getString("Answer");
                 System.out.println("ID: " + id + "Question: " + question + "Answer: " + answer);
                 
             }
          } catch ( Exception e ) {
-            System.out.println("Error: " + e.getMessage() );
+            System.out.println("Error 1: " + e.getMessage() );
          }
     }
     
-    public String getQuestion(int id) {
-       
-      return "23";  
+    public String getQuestion(final int theId) {
+        String question;
+        try {
+            this.stmt = c.createStatement();
+            
+            final ResultSet rs = stmt.executeQuery("select Question from QuestionTable where id = " + theId);  
+            question = rs.getString("Question");
+            return question; 
+            
+        } catch ( Exception e ) {
+            System.out.println("Error 2: " + e.getMessage() );
+        }
+        
+       return "There is no question";
     }
     
-    public void InsertToDB(String Q, String A) {
+    public String getAnswer(int id) {
+        String question;
+        try {
+            this.stmt = c.createStatement();
+            
+            ResultSet rs = stmt.executeQuery("select Answer from QuestionTable where id = " + id);  
+            question = rs.getString("Answer");
+            return question; 
+            
+        } catch ( Exception e ) {
+            System.out.println("Error 3: " + e.getMessage() );
+        }
+        
+       return "There is no answer";
+    }
+    
+    public void updateIsUsed(int id) {
+        try {
+            this.stmt = c.createStatement();
+            String query = "UPDATE QuestionTable SET IsUsed = 1 WHERE id=" + id;
+            stmt.executeUpdate(query);
+//            System.out.println("Updated.");
+            
+        } catch ( Exception e ) {
+            System.out.println("Error 4: " + e.getMessage() );
+        } finally {
+            closeDB();
+        }
+    }
+    
+    /**
+     * The method to check if a question used or no.
+     * 
+     * @param id is the question id.
+     * @return true if question already used
+     * @return false if question doesn't use 
+     */
+    public boolean getIsUsed(int id) {
+        final String checkForUse;
+        boolean isUsed = false;
+        try {
+            this.stmt = c.createStatement();
+            
+            ResultSet rs = stmt.executeQuery("select IsUsed from QuestionTable where id = " + id);  
+            checkForUse = rs.getString("IsUsed");
+//            System.out.println("Isused: " + isUsed);
+            if (checkForUse.equals("1")) {
+                isUsed = true;
+            }
+        } catch ( Exception e ) {
+            System.out.println("Error 5: " + e.getMessage() );
+        } 
+        return isUsed;
+    }
+    
+    public void setIsUsedToDefault() {
+        try {
+            this.stmt = c.createStatement();
+            
+            ResultSet rs = stmt.executeQuery("select * from QuestionTable");
+            String query = "UPDATE QuestionTable SET IsUsed = 0";
+            while(rs.next()) {
+                stmt.executeUpdate(query);
+//                System.out.println("Changed.");
+            } 
+            
+        } catch ( Exception e ) {
+            System.out.println("Error 6: " + e.getMessage() );
+        }
+    }
+    
+    public void insertToDB(final String theQuestion, final String theAnswer) {
         try {
             this.stmt = c.createStatement();
             String sql = "insert into QuestionTable (Question, Answer)"
-                    + " VALUES (\"" + Q + "\", \"" + A + "\");";
+                    + " VALUES (\"" + theQuestion + "\", \"" + theAnswer + "\");";
             stmt.executeUpdate(sql);        
         } catch ( Exception e ) {
-            System.out.println("Error: " + e.getMessage() );
-         }
+            System.out.println("Error 7: " + e.getMessage() );
+        }
     }
     
-    public void CloseDB() {
+    public int getLastId() {
+        int id = 0;
+        String lastRow = "SELECT * FROM QuestionTable WHERE ID = (SELECT MAX(ID) FROM QuestionTable)";
+        
+        try {
+            this.stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery(lastRow);  
+            id = rs.getInt("ID");
+            
+//            System.out.println("get last id: " +id);        
+        } catch ( Exception e ) {
+            System.out.println("Error 8: " + e.getMessage() );
+        }
+        return id;
+    }
+    
+    public void closeDB() {
         try {
             c.close();
          } catch ( Exception e ) {
-            System.out.println("Error: " + e.getMessage() );
+            System.out.println("Error 9: " + e.getMessage() );
          }
     }
     
--- src/model/Question.java
+++ src/model/Question.java
@@ -1,35 +1,70 @@
+
 package model;
 
 import java.util.Objects;
+import java.util.Random;
 
 import db.SqliteDB;
 
 /**
  * 
  * @author Zach Hunter 
+ * @author Oleksandr Maistruk
  *
  */
 public class Question {
     private String myQuestion;
     private String mySolution;
-    private SqliteDB database;
+    private SqliteDB myDatabase;
+    private final int myId;
     
     public Question() {
         //myQ = new Question();
         //getQuestion();
-        //To be changed once database is up 
         myQuestion = "What color is the sky?";
         mySolution = "blue";
-        database = new SqliteDB();
+        myDatabase = new SqliteDB();
+        myId = idHelper();
     }
     
+    /**
+     * This method is used to assign question id randomly. 
+     * 
+     * @return question id
+     */
+    private int idHelper() {
+        final Random rand = new Random();
+        final int random = rand.nextInt(myDatabase.getLastId())+1;
+//        System.out.println("random #: " + random);
+//        if (myDatabase.getIsUsed(random) == true) {
+//            idHelper();
+//        } else {
+//            myDatabase.updateIsUsed(random);
+//        }
+        
+        return random;
+    }
+//    private int idHelper() {
+//        final Random rand = new Random();
+//        final int random = rand.nextInt(myDatabase.getLastId())+1;
+//        System.out.println("random #: " + random);
+//        if (myDatabase.getIsUsed(random) == true) {
+//            idHelper();
+//        } else {
+//            myDatabase.updateIsUsed(random);
+//        }
+//        
+//        return random;
+//    }
     
     //getQuestion - say they're a string for now
     public String getQuestion() {
+        myQuestion = myDatabase.getQuestion(myId);
         return myQuestion;
     }
     
     public String getSolution() {
+        mySolution = myDatabase.getAnswer(myId);
         return mySolution;
     }
     
@@ -38,8 +73,9 @@ public class Question {
         mySolution = theSol;
     }
     
-    //Determine if input is solution 
+     
     /**
+     * Determine if input is solution
      * 
      * @param theInput
      * @return mySolution.toLowerCase().equals(theInput.toLowerCase())
@@ -54,6 +90,6 @@ public class Question {
         if (theObj == null || getClass() != theObj.getClass()) return false;
         
         Question question = (Question) theObj;
-        return Objects.equals(myQuestion, question.getQuestion()) && Objects.equals(mySolution, question.getSolution());
+        return Objects.equals(myQuestion, question.myQuestion) && Objects.equals(mySolution, question.mySolution);
     }
 }
